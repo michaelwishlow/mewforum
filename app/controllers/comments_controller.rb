b635e04ab8692eb98dfb5b91f6c70761bb0b7a15
michaelwishlow <michael@pantheritconsulting.com>
2016-11-30 13:48:48 -0800

    Add comments to Posts

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+  	@post =Post.find(params[:post_id])
+  	@comment = @post.comments.create(params[:comment].permit(:comment))
+
+  	if @comment.save
+  	  redirect_to post_path(@post)
+  	else
+  	  render 'new'
+  	end
+  end
+end
