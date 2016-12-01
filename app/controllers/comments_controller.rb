4d6734a319cc466bee6050b5bd6aeb55939ff568
michaelwishlow <michael@pantheritconsulting.com>
2016-12-01 12:29:05 -0800

    Commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
   def create
-  	@post =Post.find(params[:post_id])
-  	@comment = @post.comments.create(params[:comment].permit(:comment))
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:comment))
+		@comment.user_id = current_user.id if current_user
+		@comment.save
 
-  	if @comment.save
-  	  redirect_to post_path(@post)
-  	else
-  	  render 'new'
-  	end
-  end
-end
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render 'new'
+		end
+	end
+end	
